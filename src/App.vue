1bc4b304c8c66f725e5e46a6aa036b03abe978ac
PC <fedevr1993@gmail.com>
2020-09-21 22:26:29 -0300

    'Ejercico'

---
+++ src/App.vue
@@ -0,0 +1,71 @@
+<template>
+  <div id="app">  
+    <div class="row">
+      <div class="container-fluid col-12 bg-primary py-5">
+        <header>TVCentro</header>
+      </div>
+    </div>
+    <div class="row bg-dark">
+      <main class="container-fluid bg-light w-75 my-4">
+        <div class="row">
+          <div class="container col-6 py-2" id="formulario">
+            <form>
+              <div class="form-group m-6">
+                  <input type="text" placeholder="Nombre" class="form-control-lg w-100">
+              </div>
+              <div class="form-group m-6">
+                <input type="text" placeholder="Apellido" class="form-control-lg w-100">
+              </div>
+              <div class="form-group m-6">
+                <input type="email" placeholder="Correo electronico" class="form-control-lg w-100">
+              </div>
+              <div class="form-group m-6">
+                <select class="custom-select custom-select-lg mb-3">
+                    <option selected>Prueba</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+                <select class="custom-select custom-select-lg mb-3">
+                    <option selected>Prueba</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+              </div>
+              <div class="form-group m-6">
+                <input type="number" placeholder=Teléfono class="form-control-lg w-100">
+              </div>
+              <div class="form-group m-6">
+                <select class="custom-select custom-select-lg mb-3">
+                    <option selected>Prueba</option>
+                    <option value="1">One</option>
+                    <option value="2">Two</option>
+                    <option value="3">Three</option>
+                </select>
+              </div>
+            </form>
+          </div>
+          <div class="col-6" id="Informacion">
+            <h1>Internet 50 Megas</h1>
+            <h2>¡Conectate con la velocidad más rápida del país! Plan Internet 50 Megas WiFi + Telefonía Fija + Telecentro WiFi: Internet Gratis en la Calle. Incluye 50 Gigas Gratis por mes para que Ahorres tus Datos Móviles."</h2>
+            <span>Tu pack Incluye:</span>
+            <ul>
+              <li><b>INTERNET WIFI 50 MEGAS:</b>" Internet WiFi para tu casa."</li>
+              <li><b>Telecentro WiFi con 50G:</b>" Telecentro WiFi con 50 Gigas Gratis por Mes para que Navegues Gratis en la Calle hasta con 5 dispositivos."</li>
+              <li><b>Telefonia Fija:</b>" Telefonía Fija con 2.500 minutos libres por mes."</li>
+            </ul>
+            <span>Precio</span>
+            <div>"$ 1209" <lable>/mes</lable></div>
+            <span>Instalación</span>
+            <div>"$ 0" </div>
+          </div>
+        </div>
+      </main>
+    </div>
+    <div class="row">
+      <footer></footer>
+    </div>
+  </div>
+</script>
+
